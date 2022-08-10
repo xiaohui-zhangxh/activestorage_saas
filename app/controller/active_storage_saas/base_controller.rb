1a3a0405eb66bf8bee2d0c463f6118c30cca4b6d
张小辉 <xiaohui@tanmer.com>
2022-08-10 19:53:41 +0800

    rename directory; remove function current_tenant

--- app/controller/active_storage_saas/base_controller.rb
+++ app/controller/active_storage_saas/base_controller.rb
@@ -1,7 +1,2 @@
 class ActiveStorageSaas::BaseController < ActiveStorage::BaseController
-  unless method_defined?(:current_tenant)
-    def current_tenant
-      raise NotImplementedError
-    end
-  end
 end
